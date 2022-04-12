30f9d48353db2ce6d5ab769386ff274e11c5aa8b
Monster <1319683316@qq.com>
2022-04-12 15:48:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-04-08 16:28:21
+ * @LastEditTime: 2022-04-11 17:00:47
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \recorddemo\src\App.vue
+-->
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">路径动画demo</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
